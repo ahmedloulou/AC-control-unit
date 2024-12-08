2702dcea19535e60db7fc3d3d4473e11460df9e4
ahmedloulou <al2200307@tkh.edu.eg>
2024-12-08 13:29:40 +0200

    Update I2C.ino

--- I2C.ino
+++ I2C.ino
@@ -1,25 +1,25 @@
 #include "I2C.h"
 
 void I2C_Init(void) {
-    TWSR = 0x00;                              // Set prescaler to 1
-    TWBR = ((F_CPU / SCL_CLOCK) - 16) / 2;    // Set bit rate
-    TWCR = (1 << TWEN);                       // Enable I2C
+    TWSR = 0x00;                             
+    TWBR = ((F_CPU / SCL_CLOCK) - 16) / 2;   
+    TWCR = (1 << TWEN);                      
 }
 
 void I2C_Start(void) {
     TWCR = (1 << TWSTA) | (1 << TWEN) | (1 << TWINT);
-    while (!(TWCR & (1 << TWINT)));           // Wait for start condition
+    while (!(TWCR & (1 << TWINT)));           
 }
 
 void I2C_Stop(void) {
     TWCR = (1 << TWSTO) | (1 << TWEN) | (1 << TWINT);
-    _delay_us(10);                            // Short delay after stop
+    _delay_us(10);                          
 }
 
 void I2C_Write(uint8_t data) {
-    TWDR = data;                              // Load data into data register
-    TWCR = (1 << TWEN) | (1 << TWINT);        // Start transmission
-    while (!(TWCR & (1 << TWINT)));           // Wait for completion
+    TWDR = data;                            
+    TWCR = (1 << TWEN) | (1 << TWINT);       
+    while (!(TWCR & (1 << TWINT)));          
 }
 
 uint8_t I2C_ReadAck(void) {
